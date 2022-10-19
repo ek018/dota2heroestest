2e004cd625907b5055920a68bb211e83ee4edf9f
@eko.prasetiyo <eko.prasetiyo@sent.tv>
2022-10-19 13:36:36 +0700

    Initial Commit

---
+++ dota2heroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dota2heroes
+//
+//  Created by Eko Prasetiyo on 19/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
